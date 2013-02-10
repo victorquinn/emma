b1b7921b7a051f2600b08bf0309df07617f94db8
Victor Quinn <vquinn@ngpvan.com>
2013-02-10 12:31:47 -0500

    Add cookieParser, necessary for Sessions

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
  */
 
 var express = require('express'),
-  routes = require('./routes'),
-  user = require('./routes/user'),
-  http = require('http'),
-  path = require('path');
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
 
 /**
  * Environment variables
@@ -16,8 +16,6 @@ var express = require('express'),
 var twitter_consumer_key = process.env.TWITTER_CONSUMER_KEY,
     twitter_consumer_secret = process.env.TWITTER_CONSUMER_SECRET,
     session_secret = process.env.SESSION_SECRET;
-
-console.log(twitter_consumer_secret);
 
 var app = express();
 
@@ -28,6 +26,7 @@ app.configure(function(){
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
   app.use(express.methodOverride());
   app.use(express.session({secret: session_secret}));
   app.use(app.router);
