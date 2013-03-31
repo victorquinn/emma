2646bc340fdb49e45f9efd20649a1fa1d9fc1606
Victor Quinn <vquinn@ngpvan.com>
2013-03-31 18:02:31 -0400

    Now showing login button or profile pic and logout button in navbar

--- routes/user.js
+++ routes/user.js
@@ -10,6 +10,6 @@ var passport = require('passport'),
 
 module.exports = function(app) {
   app.get('/account', function(req, res) {
-    res.render('account', { title: "Emma | Account", user: req.user });
+    res.render('account', { title: "Emma", user: req.user });
   });
 };
